6d0d5f8a4736ef7813b4eb7d702312abce670b69
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-29 18:42:36 +0200

    added init Header

---
+++ framework/libunit.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libunit.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 18:41:34 by nweber            #+#    #+#             */
+/*   Updated: 2025/08/29 18:42:19 by nweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBUNIT_H
+# define LIBUNIT_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+
+#endif
