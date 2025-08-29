793a596f813d13b16f0b88906fa4a92e35e10765
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-08-29 20:39:47 +0200

    updates header structure

--- framework/libunit.h
+++ framework/libunit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libunit.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 18:41:34 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/29 19:16:40 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/29 20:37:54 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,17 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 
-typedef struct t_unit_test;
-
-typedef struct s_unit_test
+typedef struct t_test_suite;
+typedef struct s_test_suite
 {
-	char		test_name[256];
-	void		*(*f)(void*);
-	t_unit_test	*next;
-}				t_unit_test;
+	char			*name;
+	int				(*f)(int c);
+	int				expected;
+	int				result;
+	t_test_suite	*next;
+}					t_test_suite;
 
-void	load_test(t_unit_test *unit_test, char *test_name, void *(*f)(void*));
-void	launch_tests(void);
+void	load_test(t_test_suite *test_suite, char *test_name, int (*f)(int c));
+void	launch_tests(t_test_suite *test_suite);
 
 #endif
