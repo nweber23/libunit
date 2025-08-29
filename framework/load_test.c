8aeaa656ba174ab241c4d30e06b01316e35bd725
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-08-29 19:09:33 +0200

    adds empty function source files

---
+++ framework/load_test.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 18:55:24 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/08/29 18:56:08 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+
+void	load_test(t_unit_test *unit_test, char *test_name, void *(*f)(void*))
+{
+	(void)unit_test;
+	(void)test_name;
+	(void)f;
+}
