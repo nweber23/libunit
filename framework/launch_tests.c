8eec5061c821e8f003b27897634b281a0b2a81d8
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-08-29 20:40:16 +0200

    updates arguments in functions

--- framework/launch_tests.c
+++ framework/launch_tests.c
@@ -6,12 +6,13 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 18:55:22 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/08/29 19:07:08 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/08/29 20:39:08 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
 
-void	launch_tests(void)
+void	launch_tests(t_test_suite *test_suite)
 {
+	(void)test_suite;
 }
--- framework/load_test.c
+++ framework/load_test.c
@@ -6,15 +6,15 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 18:55:24 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/08/29 18:56:08 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/08/29 20:38:53 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
 
-void	load_test(t_unit_test *unit_test, char *test_name, void *(*f)(void*))
+void	load_test(t_test_suite *test_suite, char *test_name, int (*f)(int c))
 {
-	(void)unit_test;
+	(void)test_suite;
 	(void)test_name;
 	(void)f;
 }
