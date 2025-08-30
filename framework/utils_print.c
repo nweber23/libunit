d477a1bf19969bc256630b62a20720db02ec2d67
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-08-30 18:23:41 +0200

    refactors ftu_print_final_result()

--- framework/utils_print.c
+++ framework/utils_print.c
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 12:01:08 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/08/30 16:50:41 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/08/30 18:12:18 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void	ftu_print_error_fd(char *error_msg, char *test_name, int fd)
 	ft_putendl_fd("\033[0m", fd);
 }
 
-// [test_function]:[test_name]:[status]
 void	ftu_print_result(char *func_name, t_unit_test *unit_test)
 {
-	static char	status_msg[4][32] = {LIGHTGREEN"[OK]"DEFAULT \
-		, LIGHTRED"[KO]"DEFAULT, YELLOW"[SIGV]"DEFAULT, \
-		YELLOW"[BUSE]"DEFAULT};
+	static char	status_msg[4][32] = {LIGHTGREEN"[OK]"DEFAULT, \
+		LIGHTRED"[KO]"DEFAULT, \
+		LIGHTYELLOW"[SIGV]"DEFAULT, \
+		LIGHTYELLOW"[BUSE]"DEFAULT};
 
 	ft_putstr_fd(func_name, 1);
 	ft_putstr_fd(": ", 1);
@@ -52,8 +52,12 @@ void	ftu_print_final_result(void)
 	if (ftu_count_routine_result(0, 0, false))
 	{
 		ft_putstr_fd(GREEN, 1);
-		ft_putendl_fd(ALL_TESTS_PASSED_MSG, 1);
-		ft_putstr_fd(DEFAULT, 1);
+		ft_putstr_fd(GREEN"===============================================", 1);
+		ft_putendl_fd(DEFAULT, 1);
+		ft_putendl_fd("  🎉🥳"LIGHTGREEN BOLD"All tests passed!" REGULAR \
+			GREEN " Congratulations! 🥳🎉  ", 1);
+		ft_putstr_fd(GREEN"===============================================", 1);
+		ft_putendl_fd(DEFAULT, 1);
 		return ;
 	}
 }
