060ee2b602c4b16800db9513c0b3fa54ef326826
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-30 19:50:22 +0200

    added some small ascii arts to the code

--- real-tests/main.c
+++ real-tests/main.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 20:43:50 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/30 19:06:42 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/30 19:49:48 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int	main(void)
 {
-	ft_putstr_fd(LIGHTCYAN""BOLD"Let the testing begin ...\n\n"RESET, 1);
+	ft_putstr_fd(LIGHTCYAN""BOLD"-----------«««<< LETS TEST >>»»»-----------\n\n"RESET, 1);
 	ft_isalpha_launcher();
 	ft_strlen_launcher();
 	ft_strcmp_launcher();
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 16:58:49 by nweber            #+#    #+#             */
-/*   Updated: 2025/08/30 17:26:20 by nweber           ###   ########.fr       */
+/*   Updated: 2025/08/30 19:48:59 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	main(void)
 {
-	ft_putstr_fd(LIGHTCYAN""BOLD"DUMMY TESTING\n\n"RESET, 1);
+	ft_putstr_fd(LIGHTCYAN""BOLD"---------«««<< DUMMY TESTING >>»»»---------\n\n"RESET, 1);
 	ft_dummy_launcher();
 	ftu_print_final_result();
 	return (0);
