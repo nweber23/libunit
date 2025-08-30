71d451c25636a8492d51f290e6e19572503088be
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-08-30 15:36:54 +0200

    counts tests and passed tests of test routine

--- framework/launch_tests.c
+++ framework/launch_tests.c
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 18:55:22 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/08/30 15:01:37 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/08/30 15:34:15 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ static int	exec_unit_test(t_unit_test *unit_test)
 
 int	launch_tests(char *func_name, t_list *lst)
 {
+	static int	count;
+	static int	passed;
 	t_list		*curr;
 	t_unit_test	*unit_test;
 
@@ -46,9 +48,17 @@ int	launch_tests(char *func_name, t_list *lst)
 	while (curr)
 	{
 		unit_test = (t_unit_test *)(curr->content);
+		++count;
 		unit_test->status = exec_unit_test(unit_test);
+		if (unit_test->status == 0)
+			++passed;
 		ftu_print_result(func_name, unit_test);
 		curr = curr->next;
 	}
+	ft_putnbr_fd(passed, 1);
+	ft_putstr_fd("/", 1);
+	ft_putnbr_fd(count, 1);
+	ft_putendl_fd(" tests checked", 1);
+	ft_putendl_fd("", 1);
 	return (0);
 }
